bb4e9cd500285f31d4c0b4b83a5961b8ae081e6c
mikepasoglou <mp@carparking.gr>
2023-12-04 18:44:25 +0200

    Initial commit

---
+++ Client/wwwroot/js/myScript.js
@@ -0,0 +1,25 @@
+﻿function changeheader() {
+    const heading = document.createElement('h1');
+    const headingText = document.createTextNode('Big Head NO 222222222!');
+    heading.appendChild(headingText);
+    document.body.appendChild(heading);
+}
+
+function showAlert() {
+    alert("message");
+}
+
+
+
+
+function changecssstyle() {
+
+    var element = document.getElementById("awesome");
+    element.style.width = "min-content";
+}
+
+
+
+
+
+
